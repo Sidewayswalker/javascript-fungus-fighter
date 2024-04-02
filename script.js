06b7a9e0f51b04ba9aa2aededd7c0b05c2ca1215
Steven Karl <Karl.Steven09@gmail.com>
2024-04-02 00:19:22 -0500

    got the status bars to move.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
+let userAP = 100;
 
 function onReady() {
     console.log("Ready to go!")
@@ -16,5 +17,49 @@ function onReady() {
     // - Rendered to the DOM
 }
 
+function arcaneScepterAttack(event){
+    // fungusHP -= 14;
+    // userAP -= 12
+    if(fungusHP > 0 && userAP >= 12){
+        let currentAP = document.getElementsByClassName("ap-text")
 
-onReady()
+        let currentApMeter = document.getElementById("ap-meter")
+        currentApMeter.value -= 12
+
+        // let currentHP = document.getElementsByClassName("hp-text")
+        let currentHP = document.getElementById("HP-NUMBER");
+        let updatedHP = (Number(currentHP.innerHTML) - 14)
+
+        let currentHpMeter = document.getElementById("hp-meter")
+        currentHpMeter.value -= 14
+    }
+}
+
+
+// let currentHP = document.getElementById("HP-NUMBER").innerHTML;
+// currentHP.innerHTML - 14
+
+
+
+
+
+// let currentHP = document.querySelector('.hp-text');
+// let textContentHP = currentHP.textContent
+// let parts = textContentHP.split(' ');
+// console.log(parts[0] - 14)
+
+
+
+
+
+
+
+// function entangleAttack(){
+
+// }
+// function dragonBladeAttack(){
+
+// }
+// function starFireAttack(){
+
+// }
